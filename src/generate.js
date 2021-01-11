1205b687de7d4c872545e3884dd7c6f3cc93918d
J-Kowalewski <44169160+J-Kowalewski@users.noreply.github.com>
2021-01-11 18:14:40 +0100

    zad 1

---
+++ src/generate.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function generate(){
+    const numbers = [];
+    for(let i = 0; i < 9 ; i++){
+        numbers[i] = Math.floor(Math.random() * 1000);
+    }
+
+    const listItems = numbers.map((number) =>
+        <li>{number}</li>
+    );
+    return(listItems);
+}
